309f2c2cd83b47425fef9bfa7b558673d3906d07
AngeloDallacorteEuzebio <imprimir.imbe@gmail.com>
2025-04-02 10:44:10 -0300

    Adicionando a classe Retangulo e ajustes no redimensionamento

---
+++ Retangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Criando instâncias usando todos os construtores
+        Retangulo retangulo1 = new Retangulo();
+        Retangulo retangulo2 = new Retangulo(5);
+        Retangulo retangulo3 = new Retangulo(4, 6);
+
+        // Exibindo informações iniciais
+        Console.WriteLine("Retângulo 1: Área = {0}, Perímetro = {1}", retangulo1.CalcularArea(), retangulo1.CalcularPerimetro());
+        Console.WriteLine("Retângulo 2: Área = {0}, Perímetro = {1}", retangulo2.CalcularArea(), retangulo2.CalcularPerimetro());
+        Console.WriteLine("Retângulo 3: Área = {0}, Perímetro = {1}", retangulo3.CalcularArea(), retangulo3.CalcularPerimetro());
+
+        // Testando os métodos de redimensionamento
+        retangulo3.Redimensionar(2);
+        Console.WriteLine("Retângulo 3 Redimensionado: Área = {0}, Perímetro = {1}", retangulo3.CalcularArea(), retangulo3.CalcularPerimetro());
+
+        retangulo2.Redimensionar(10, 15);
+        Console.WriteLine("Retângulo 2 Redimensionado: Área = {0}, Perímetro = {1}", retangulo2.CalcularArea(), retangulo2.CalcularPerimetro());
+    }
+}
+
---
+++ Retangulo/Retangulo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Retangulo
+    {
+    public double altura { get; set; }
+    public double largura { get; set; }
+
+    public Retangulo()
+    {
+        largura = 0;
+        altura = 0;
+    }
+    public Retangulo(double valor)
+    {
+        altura = valor;
+        largura = valor;
+    }
+    public Retangulo(double Largura, double Altura)
+    {
+        altura = Altura;
+        largura = Largura;
+    }
+
+    public double CalcularArea()
+    { 
+        return largura * altura;
+    }
+
+    public double CalcularPerimetro()
+    {
+        return 2 * (largura + altura);
+    }
+
+    public void Redimensionar()
+    {
+
+    }
+    public void Redimensionar(double fator)
+    {
+      largura *= fator; 
+      altura *= fator;
+    }
+
+    public void Redimensionar(double novaLargura, double novaAltura)
+    {
+        largura = novaLargura;
+        altura = novaAltura;
+    }
+}
